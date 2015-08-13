4c3f988a97ea736136a2c8eceb5babd30eb063f9
wulujunwlj <wulujunwlj@163.com>
2015-08-14 07:09:09 +0800

    jade syntax

--- routes/projects/front.end.navigation.js
+++ routes/projects/front.end.navigation.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 router.get('/', function(req, res) {
-	res.render('projects/front-end-navigation/index', {'username': 'abc'});
+	fs.readFile(__dirname + '../../../public/javascript/projects/front-end-navigation/data/index.json', 
+		function(err, data) {
+			if(err) throw err;
+				res.render('projects/front-end-navigation/index', {
+					title: 'title111',
+					list: data
+				});
+		});
+
 });
 
 router.get('/aaa', function(req, res) {
