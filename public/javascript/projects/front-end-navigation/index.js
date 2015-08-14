e073b8f86df693b20bc80480f0480a7e6e09a101
wulujunwlj <wulujunwlj@163.com>
2015-08-14 13:46:46 +0800

    update front-end-navigation

--- public/javascript/projects/front-end-navigation/index.js
+++ public/javascript/projects/front-end-navigation/index.js
@@ -1,19 +1,8 @@
 'use strict';
 
 $(document).ready(function() {
-	console.log(111111111111);
-
-	$.ajax({
-		type: 'GET',
-		url: 'javascript/projects/front-end-navigation/data/index.json',
-		dataType: 'text',
-		success: function(data) {
-			// console.log(data);
-			console.log($.parseJSON(data));
-		},
-		error: function(msg) {
-			console.error(msg);
-		}
+	$('.block-title').click(function() {
+		console.log($('.block-content').length);
 	});
 
 });
--- routes/projects/front.end.navigation.js
+++ routes/projects/front.end.navigation.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+
+// var _User = new Schema({
+// 	email: String,
+// 	name: String,
+// 	salt: String,
+// 	password: String
+// });
 
 router.get('/', function(req, res) {
 	fs.readFile(__dirname + '../../../public/javascript/projects/front-end-navigation/data/index.json', 
 		function(err, data) {
 			if(err) throw err;
 				res.render('projects/front-end-navigation/index', {
-					title: 'title111',
-					list: data
+					title: '首页',
+					list: JSON.parse(data)
 				});
 		});
 
